e3f398cd7f620f950f7cde965b0b2eeda3ae84d0
Adrian Dobrică <adriancnmv@gmail.com>
2016-04-12 16:09:24 +0300

    Add mangler stub

---
+++ src/base/mangler.h
@@ -0,0 +1,47 @@
+#ifndef MANGLER_H_
+#define MANGLER_H_
+
+class Mangler
+{
+public:
+    Mangler();
+    virtual ~Mangler();
+
+private:
+    class Interpreter
+    {
+    public:
+        Interpreter();
+
+        void* getFileHeader();
+        void* getProgramHeader();
+        void* getSectionHeader();
+        void* getSection();
+        void* getSymbol();
+        void* getString();
+        void* getVariable();
+        void* getHexa();
+
+        virtual ~Interpreter();
+    };
+
+    class Modifier
+    {
+    public:
+        Modifier();
+
+        bool alterFileHeader();
+        bool alterSection();
+        bool alterSymbol();
+        bool alterString();
+        bool alterVariable();
+        bool alterFree();
+
+        virtual ~Modifier();
+    };
+
+    Interpreter interpreter;
+    Modifier modifier;
+};
+
+#endif
