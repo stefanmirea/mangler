a81612ff491793a7a26e191d7abee1663edd22be
Adrian Dobrică <adriancnmv@gmail.com>
2016-04-12 18:40:03 +0300

    Add assembly stub classes

---
+++ src/base/file_assembly.cpp
@@ -0,0 +1,23 @@
+#include "file_assembly.h"
+
+FileAssembly::FileAssembly() {}
+
+FileAssembly::~FileAssembly() {}
+
+FileAssembly::Disassembler::Disassembler() {}
+
+FileAssembly::Disassembler::~Disassembler() {}
+
+std::string FileAssembly::Disassembler::disassembleCode(uint8_t* code)
+{
+    return std::string("");
+}
+
+FileAssembly::Assembler::Assembler() {}
+
+FileAssembly::Assembler::~Assembler() {}
+
+uint8_t* FileAssembly::Assembler::assembleCode(std::string& code)
+{
+    return nullptr;
+}
