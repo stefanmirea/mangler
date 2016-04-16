0eba995f8e618afb1289f75107952fa0501b22f0
Ştefan-Gabriel Mirea <mireastefangabriel@gmail.com>
2016-04-16 18:36:23 +0300

    Make state independent methods static

--- include/search_engine.hpp
+++ include/search_engine.hpp
@@ -15,9 +15,11 @@ class SearchEngine
 public:
     SearchEngine();
 
-    int getHexOffset(FileUnit &file, unsigned int start, Direction dir, std::string &searchInput);
-    int getDecOffset(FileUnit &file, unsigned int start, Direction dir, int searchInput);
-    int getStrOffset(FileUnit &file, unsigned int start, Direction dir, std::string &searchInput);
+    static int getHexOffset(FileUnit &file, unsigned int start, Direction dir,
+                            std::string &searchInput);
+    static int getDecOffset(FileUnit &file, unsigned int start, Direction dir, int searchInput);
+    static int getStrOffset(FileUnit &file, unsigned int start, Direction dir,
+                            std::string &searchInput);
 
     virtual ~SearchEngine();
 };
