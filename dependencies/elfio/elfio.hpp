6353c93dcc7b2b36d6522583a53663090afe1235
Adrian Dobrică <adriancnmv@gmail.com>
2016-05-11 11:39:42 +0300

    Add getter and setter for shnum and phnum

--- dependencies/elfio/elfio.hpp
+++ dependencies/elfio/elfio.hpp
@@ -201,6 +201,8 @@ class elfio
     ELFIO_HEADER_ACCESS_GET_SET( Elf64_Off,     sections_offset        );
     ELFIO_HEADER_ACCESS_GET_SET( Elf64_Off,     segments_offset        );
     ELFIO_HEADER_ACCESS_GET_SET( Elf_Half,      section_name_str_index );
+    ELFIO_HEADER_ACCESS_GET_SET( Elf_Half,      segments_num           );
+    ELFIO_HEADER_ACCESS_GET_SET( Elf_Half,      sections_num           );
 
 //------------------------------------------------------------------------------
     const endianess_convertor& get_convertor() const
